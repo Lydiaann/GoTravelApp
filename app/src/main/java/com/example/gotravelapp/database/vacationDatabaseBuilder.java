0051d8704cf02dfead45bfb8404c26222dddc528
Lydia <105086251+Lydiaann@users.noreply.github.com>
2025-07-03 13:53:06 -0400

    Adds userDAO to vacationDatabaseBuilder.java (line 22). Updates database version (line 17).

--- app/src/main/java/com/example/gotravelapp/database/vacationDatabaseBuilder.java
+++ app/src/main/java/com/example/gotravelapp/database/vacationDatabaseBuilder.java
@@ -8,15 +8,18 @@ import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 import com.example.gotravelapp.dao.ExcursionDAO;
+import com.example.gotravelapp.dao.UserDAO;
 import com.example.gotravelapp.dao.VacationDAO;
 import com.example.gotravelapp.entities.Excursion;
+import com.example.gotravelapp.entities.User;
 import com.example.gotravelapp.entities.Vacation;
 
-@Database(entities = {Vacation.class, Excursion.class}, version = 12, exportSchema = false)
+@Database(entities = {Vacation.class, Excursion.class, User.class}, version = 16, exportSchema = false)
 
 public abstract class vacationDatabaseBuilder extends RoomDatabase {
     public abstract VacationDAO vacationDAO();
     public abstract ExcursionDAO excursionDAO();
+    public abstract UserDAO userDao();
     private static volatile vacationDatabaseBuilder INSTANCE;
 
     static vacationDatabaseBuilder getDatabase(final Context context){
